0ae531b05784f8ded56b0a62f37faa89d17d4dec
Kpine-MO <chrispine.oduor@student.moringaschool.com>
2022-08-19 13:24:49 +0300

    commit tree dbd2571eef9dc94ba5839d2fa6363892c30ab417

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arry)
+    arry.map do |name|
+      "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(arry)
+
+  room = 0
+
+  arry.map do |name|
+
+    room += 1
+    
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+
+  end
+end
+
+def printer(arry)
+   batch_badge_creator(arry).each { |p| puts p}
+   assign_rooms(arry).each { |p| puts p}
+end
